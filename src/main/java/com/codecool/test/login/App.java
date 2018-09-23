07bf126e5886474117e8a50d9b6fa9094ad6692d
Tomasz Łukaszyk <el.moue@gmail.com>
2018-09-23 18:39:21 +0200

    Add App class handling the server

---
+++ src/main/java/com/codecool/test/login/App.java
@@ -0,0 +1,16 @@
+package com.codecool.test.login;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        HttpServer httpServer = HttpServer.create(new InetSocketAddress(8000), 0);
+        httpServer.createContext("/login", new Login());
+        httpServer.createContext("/static", new Static());
+        httpServer.setExecutor(null);
+        httpServer.start();
+    }
+}
